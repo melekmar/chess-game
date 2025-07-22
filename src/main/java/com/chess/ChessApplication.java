65cb1e1d5e5da1315ffb6647218afd41c122773d
melekmar <melek7.maraoui@gmail.com>
2025-07-23 01:12:56 +0200

    Move ChessApplication.java to correct package path

---
+++ src/main/java/com/chess/ChessApplication.java
@@ -0,0 +1,14 @@
+package com.chess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main entry point for the Chess Game Spring Boot application.
+ */
+@SpringBootApplication
+public class ChessApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChessApplication.class, args);
+    }
+}
